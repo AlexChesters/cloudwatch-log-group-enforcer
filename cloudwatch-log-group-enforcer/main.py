122defb03c32968a2f7dce4d9fa6e87876b23a5d
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-04 21:38:48 +0000

    fetch all log groups

--- cloudwatch-log-group-enforcer/main.py
+++ cloudwatch-log-group-enforcer/main.py
@@ -9,5 +9,26 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 import boto3
 
 
+client = boto3.client("logs", region_name="eu-west-1")
+log_groups = []
+
+
+def fetch_log_groups(nextToken=None):
+    if nextToken:
+        response = client.describe_log_groups(nextToken=nextToken)
+    else:
+        response = client.describe_log_groups()
+
+    for group in response["logGroups"]:
+        log_groups.append(group)
+
+    if "nextToken" in response:
+        print("next token found")
+        fetch_log_groups(response["nextToken"])
+    else:
+        return
+
+
 def handler(event, context):
-    print("Hello, world!")
+    fetch_log_groups()
+    print("found {0} log groups".format(len(log_groups)))
