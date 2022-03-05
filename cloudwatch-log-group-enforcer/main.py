731163ebff268945c1abbfee3fb633c9ed019199
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-03-05 21:22:09 +0000

    run against all regions

--- cloudwatch-log-group-enforcer/main.py
+++ cloudwatch-log-group-enforcer/main.py
@@ -7,20 +7,35 @@ if 'LAMBDA_TASK_ROOT' in os.environ:
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 import boto3
+from boto3.session import Session
+from botocore.exceptions import ClientError
+
 
 DEFAULT_RETENTION = 3
-REGIONS = [
-    "eu-west-1",
-    "us-east-1"
-]
 
 
 def flatten(l):
     return [item for sublist in l for item in sublist]
 
 
+def region_is_available(region):
+    sts = boto3.client('sts', region_name=region)
+    try:
+        sts.get_caller_identity()
+        return True
+    except ClientError as e:
+        return False
+
+
 def handler(event, context):
-    for region in REGIONS:
+    s = Session()
+    regions = s.get_available_regions("cloudwatch")
+
+    for region in regions:
+        if not region_is_available(region):
+            print("region {0} is not enabled, skipping it".format(region))
+            continue
+
         client = boto3.client("logs", region_name=region)
         paginator = client.get_paginator("describe_log_groups")
 
