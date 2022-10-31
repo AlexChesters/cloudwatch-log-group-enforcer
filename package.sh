b11e339c407b47f7994cdd8a6f715a62f25b4cde
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-10-31 09:59:52 +0000

    fix build (hopefully)

--- package.sh
+++ package.sh
@@ -4,6 +4,8 @@ export PATH="${HOME}/.poetry/bin:${PATH}"
 export VENV_PATH=$(poetry env info -p)
 
 poetry install --only main
-mkdir build
-cp -R ${VENV_PATH}/lib/python3.*/site-packages/* build/
-cp -R cloudwatch_log_group_enforcer/ build/
+
+poetry build --format wheel
+pip install ./dist/*.whl -t lambda_dist
+
+cd lambda_dist && zip lambda * -r
